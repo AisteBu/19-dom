5d33d5bc17bc5690c0b0e786c3fba99caea9c777
AisteB <aistebuneviciene@gmail.com>
2022-05-22 15:55:07 +0200

    PavadinimaiDidziosiomArray

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 /*
 Surasti visus ingridientus ir ju pavadinimus atspausdinti i console
 
+Visu ingridientu pavadinimai turi buti didziosiomis raidemis ir sudeti i array
+
+Isspausdinti gauta array i console
+
+Norimas rezultatas:
+['MORKOS', 'BULVES', 'SVOGUNAS', 'VANDUO', 'DRUSKA']
 */
 
 const liDOM = document.querySelectorAll('li');
-console.log(liDOM);
+const ingridientai = [];
 
 for (let i = 0; i < liDOM.length; i++) {
-    const ingridientas = liDOM[i].innerText;
-    console.log(ingridientas);
+    const ingridientas = liDOM[i].innerText.toUpperCase();
+    ingridientai.push(ingridientas);                // prilipdome nauja irasa is galo . itraukiame nauja ingridienta i sarasa
 }
+console.log(ingridientai);
+
