a04e9a2ffbb6cdb022725e3a39f366f1d1cca91b
AisteB <aistebuneviciene@gmail.com>
2022-05-22 17:32:05 +0200

    RastiIngridientusPridetiAtitinkamaClass

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 /*
 Surasti h1 (antraste)
 Prideti prie jos CSS class, kuri pakeicia teksto spalva i raudona
+
+Surasti visus ingridientus
+Kiekvienam ingridientui prideti CSS class pavadinimu darzove
 */
 
 const h1DOM = document.querySelector('h1');     // susirandam darbini elementa, su kuriuo norime dirbti
 h1DOM.classList.add('antraste');                // kreiptis i nurodyta elementa ir prie jo klasiu saraso prideti nurodyto pavadinimo klase (tada h1 pasidarys toks, koki klase turi aprasyma)
 
+const ulDOM = document.querySelector('ul');
+ulDOM.classList.add('darzove');
+
+
+
+ 
