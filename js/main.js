64d9ee352bf162a5b9aa85b8a7147930049d77ea
AisteB <aistebuneviciene@gmail.com>
2022-05-22 18:44:05 +0200

    DarzovesTekstasClass4

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 /*
 Surasti h1 (antraste)
-Perrasyti h1 elemento teksta i "Zuikio tekstas"
+Perrasyti h1 elemento teksta i "Zuikio pomidoru darzas"
+Padaryk, kad h1 elementas turetu CSS class "pavadinimas"
 
 Surasti VISAS darzoves
-Kiekvienos darzoves teksta pakeisti i "Israuta darzove"
+Kiekvienos darzoves teksta pakeisti i "Pomidoras"
+Kiekvienai darzovei suteikti CSS class "pomidoras"
 */
 
 const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Zuikio darzas';
+h1DOM.innerText = 'Zuikio pomidoru darzas';
+h1DOM.classList.add('pavadinimas');
 
-const ingridientai = document.querySelectorAll('li');
-for (let i = 0; i < ingridientai.length; i++) {
-    ingridientai[i].innerText = 'Israuta darzove';              // jei vietoj i irasysime skaiciu, tai israus tik numeriuku pazymeta darzove
+
+const darzovesDOM = document.querySelectorAll('li');
+for (let i = 0; i < darzovesDOM.length; i++) {
+    darzovesDOM[i].innerText = 'Pomidoras';
+    darzovesDOM[i].classList.add('pomidoras');
 }
 
 
