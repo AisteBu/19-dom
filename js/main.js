932d538af36285de31541242d2296ad4b2a3aa89
AisteB <aistebuneviciene@gmail.com>
2022-05-23 05:56:35 +0200

    Hamburger event listener

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
 /*
-Surasti h1 (antraste)
-Perrasyti h1 elemento teksta i "Zuikio pomidoru darzas"
-Padaryk, kad h1 elementas turetu CSS class "pavadinimas"
+Surasti logotipa
 
-Surasti VISAS darzoves
-Kiekvienos darzoves teksta pakeisti i "Pomidoras"
-Kiekvienai darzovei suteikti CSS class "pomidoras"
+Susirasti hamburgeri
+
+Paspaudus ant hamburgerio, logotipo elementui reikia uzdeti CSS class pavadinimu "big"
+
+Antra karta paspaudus ant hamburgerio, logotipo elementui reikia nuimti CSS class pavadinimu "big"
+
 */
 
-const h1DOM = document.querySelector('h1');
-h1DOM.innerText = 'Zuikio pomidoru darzas';
-h1DOM.classList.add('pavadinimas');
+const logoDOM = document.querySelector('.logo');
+const hamburgerDOM = document.querySelector('.hamburger');
 
+console.log(logoDOM);
+console.log(hamburgerDOM);
 
-const darzovesDOM = document.querySelectorAll('li');
-for (let i = 0; i < darzovesDOM.length; i++) {
-    darzovesDOM[i].innerText = 'Pomidoras';
-    darzovesDOM[i].classList.add('pomidoras');
-}
-
-
- 
+hamburgerDOM.addEventListener('click', () => {
+    logoDOM.classList.toggle('big');            /* toggle -> jeigu yra - nuimk, jei nera - pridek */
+} )
