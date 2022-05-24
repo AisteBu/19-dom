76911c683c42d53a78229e0c9495b56ce3cf8f24
AisteB <aistebuneviciene@gmail.com>
2022-05-24 05:26:10 +0200

    HamburgerEventListener2

--- js/main.js
+++ js/main.js
@@ -7,14 +7,31 @@ Paspaudus ant hamburgerio, logotipo elementui reikia uzdeti CSS class pavadinimu
 
 Antra karta paspaudus ant hamburgerio, logotipo elementui reikia nuimti CSS class pavadinimu "big"
 
+
+Susirasti meniu elementa (nav)
+
+Paspaudus ant hamburgerio, meniu elementui reikia uzdeti CSS class pavadinimu "visible"
+
+Antra karta paspaudus ant hamburgerio, meniu elementui reikia nuimti CSS class pavadinimu "visible"
+
+
+Extra:
+CSS perrasyti taip, jog meniu elementai butu atvaizduoti vertikaliai ir logo su hamburgeriu - "kabetu" virsuje
 */
 
-const logoDOM = document.querySelector('.logo');
-const hamburgerDOM = document.querySelector('.hamburger');
+// const logoDOM = document.querySelector('.logo');
+// const hamburgerDOM = document.querySelector('.hamburger');
 
-console.log(logoDOM);
-console.log(hamburgerDOM);
+// console.log(logoDOM);
+// console.log(hamburgerDOM);
+
+// hamburgerDOM.addEventListener('click', () => {
+//     logoDOM.classList.toggle('big');            /* toggle -> jeigu yra - nuimk, jei nera - pridek */
+// } )
+
+const hamburgerDOM = document.querySelector('.hamburger');
+const navDOM = document.querySelector('nav');
 
 hamburgerDOM.addEventListener('click', () => {
-    logoDOM.classList.toggle('big');            /* toggle -> jeigu yra - nuimk, jei nera - pridek */
-} )
+    navDOM.classList.toggle('visible');
+})
