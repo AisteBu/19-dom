1883d9f0798456a00e18d52c2936a83395d92fb1
AisteB <aistebuneviciene@gmail.com>
2022-05-26 11:57:25 +0200

    PoSubmitTekstasIsInputISpan

--- js/main.js
+++ js/main.js
@@ -8,35 +8,49 @@
 
 // Kiekviena karta spaudziant reset mygtuka reikia, jog zinuciu sarase atsirastu irasas su tekstu "Paspaudei reset ir dabartinis rezultatas yra [skaicius]."
 
-let rezultatas = 0;             // jeigu butu const, negaletume atnaujinti rezultato
+// let rezultatas = 0;             // jeigu butu const, negaletume atnaujinti rezultato
 
-const minusDOM = document.querySelector('.minus');
-const numberDOM = document.querySelector('.number');
-const plusDOM = document.querySelector('.plus');
-const resetDOM = document.querySelector('.reset');
-const h1DOM = document.querySelector('h1');
-const ulDOM = document.querySelector('ul');
+// const minusDOM = document.querySelector('.minus');
+// const numberDOM = document.querySelector('.number');
+// const plusDOM = document.querySelector('.plus');
+// const resetDOM = document.querySelector('.reset');
+// const h1DOM = document.querySelector('h1');
+// const ulDOM = document.querySelector('ul');
 
-const gameInProgressMessage = 'Žaidimas progrese';
-const initialH1Text = h1DOM.innerText;
+// const gameInProgressMessage = 'Žaidimas progrese';
+// const initialH1Text = h1DOM.innerText;
 
-ulDOM.innerHTML = `<li> Tu pasileidai žaidimą - sekmės! </li>`;
+// ulDOM.innerHTML = `<li> Tu pasileidai žaidimą - sekmės! </li>`;
 
-minusDOM.addEventListener('click', () => {
-    numberDOM.innerText = --rezultatas;
-    h1DOM.innerText = gameInProgressMessage;
-    ulDOM.innerHTML += `<li> Paspaudei minusą ir dabartinis rezultatas yra ${rezultatas}. </li>`
-})
+// minusDOM.addEventListener('click', () => {
+//     numberDOM.innerText = --rezultatas;
+//     h1DOM.innerText = gameInProgressMessage;
+//     ulDOM.innerHTML += `<li> Paspaudei minusą ir dabartinis rezultatas yra ${rezultatas}. </li>`
+// })
 
-plusDOM.addEventListener('click', () => {
-    numberDOM.innerText = ++rezultatas;
-    h1DOM.innerText = gameInProgressMessage;
-    ulDOM.innerHTML += `<li> Paspaudei pliusą ir dabartinis rezultatas yra ${rezultatas}. </li>`
-})
+// plusDOM.addEventListener('click', () => {
+//     numberDOM.innerText = ++rezultatas;
+//     h1DOM.innerText = gameInProgressMessage;
+//     ulDOM.innerHTML += `<li> Paspaudei pliusą ir dabartinis rezultatas yra ${rezultatas}. </li>`
+// })
 
-resetDOM.addEventListener('click', () => {
-    rezultatas = 0;
-    numberDOM.innerText = rezultatas;
-    h1DOM.innerText = initialH1Text;
-    ulDOM.innerHTML += `<li> Paspaudei reset ir dabartinis rezultatas yra ${rezultatas}. </li>`
+// resetDOM.addEventListener('click', () => {
+//     rezultatas = 0;
+//     numberDOM.innerText = rezultatas;
+//     h1DOM.innerText = initialH1Text;
+//     ulDOM.innerHTML += `<li> Paspaudei reset ir dabartinis rezultatas yra ${rezultatas}. </li>`
+// })
+
+// Formoje parasytas tekstas, po `submit` mygtuko paspaudimo turi buti atvaizduotas `<span>` elemente.
+
+// `input` elemento reiksme istraukiama kreipiantis i to elemento `value` parametra, pvz.: `element.value`
+
+const inputDOM = document.querySelector('input');
+const buttonDOM = document.querySelector('button');
+const spanDOM = document.querySelector('span');
+
+
+buttonDOM.addEventListener ('click', (event) => {    //event - tai tik kintamasis
+    event.preventDefault();
+    spanDOM.innerText = inputDOM.value;
 })
